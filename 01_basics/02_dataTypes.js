97d00a953c67d6d0331303e3073462eabf570f46
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-05-15 02:57:01 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("3+3"); // we are using node.js not browser
+
+// code readability should be a priority
+// use mdn for reference or ECMA script language Specification
+
+
+let name = "Hamza"; // String
+let age = 25; // numercial
+let isloggedIn = false; // boolean
+//bigInt - for large number
+// null = standalone value - is an object when called 
+// undefined = value not defined but variable is declared 
+// symbol = unique
+
+/// object 
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
