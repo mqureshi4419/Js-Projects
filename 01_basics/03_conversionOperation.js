4f4290dc4d619d127604d50ce8f809152c14e833
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-05-15 03:11:07 +0000

    conversion of DataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = true;
+
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+// "33" = 33
+// "33abc" -> NaN
+// true -> 1; false -> 0
+
+
+let isloggedIn = "Hamza";
+
+let booleanIsLoggedIn = Boolean(isloggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// "" -> false
+// "Hamza" -> true
+
+let someNumber = 33;
+
+let StringNumber = String(someNumber);
+
+console.log(StringNumber);
