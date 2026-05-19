5405ca44cfeb3d38fbde0574c8b547da92ad07f5
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-05-19 19:22:13 +0000

    dataType - SUmmary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,58 @@
+// Primitive Data Type
+// 7 types : String , Number , Boolean, null, undefined, Symbol, bigInt
+
+const score = 100;
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 334838823838838223232n
+
+
+// Reference Type (Non Primitive)
+//  Array, Objects (Master this), Functions
+
+const heros = ["SpiderMan", "Thor", "IronMan"]
+let myObj = {
+    name: "Hamza",    // Objects
+    age: 22,
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+
+console.log(typeof id); // 
+
+/**
+     1.Number:
+        Represents both integers and floating-point numbers.
+        Typical Range: -9,007,199,254,740,992 (-2^53) to 9,007,199,254,740,992 (2^53) inclusive.
+        Smallest Increment: 2^(-52).
+
+    2.String:
+        Represents a sequence of characters.
+        No specific range limit, but practical limits depend on memory and system resources.
+
+    3.Boolean:
+        Represents true or false.
+        Only two possible values: true and false.
+
+    4.Undefined:
+        Represents a variable that has been declared but hasn't been assigned a value.
+        It has only one possible value: undefined.
+
+    5.Null:
+        Represents the intentional absence of any object or value.
+        It has only one possible value: null.
+
+    6.Symbol (ES6):
+        Represents a unique and immutable value.
+        No specific range limit.
+
+    7.BigInt (ES11):
+        Represents large integers that cannot be represented by the Number type.
+        The range is practically unlimited and depends on available memory.
+ */
