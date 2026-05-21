18e67a232587ac511c26af8868758e158784d4c8
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-05-21 20:40:43 +0000

    strings = depth

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -11,12 +11,26 @@ console.log(gameName[0]);
 console.log(gameName.__proto__);
 
 console.log(gameName.length);
-console.log(gameName.toUpperCase(gameName));
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+console.log(gameName.toUpperCase(gameName)); // all letter uppercase
+console.log(gameName.charAt(2)); // which position is the character
+console.log(gameName.indexOf('t')); // what is the character position 
 
-// learn string method
+// learn string method - important!!
 
-const newString = gameName.substring(0, 4);
+const newString = gameName.substring(0, 4); // can't give negative value
 console.log(newString);
+
+const anotherString = gameName.slice(-8,4); // slice can be give neg value
+console.log(anotherString);
+
+const newStringOne = "    Hamza    ";
+console.log(newStringOne);
+console.log(newStringOne.trim()); // trim white spaces
+
+const url = "https://Hamza.com/Hamza%20Hammy";
+console.log(url.replace('%20', '-')); // for replace
+console.log(url.includes('cute')); // validate if the char is there
+
+console.log(gameName.split('-'));
+
 
