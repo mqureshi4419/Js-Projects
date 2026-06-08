e376c1de7b983f360110a07e844ddf6ea26e6976
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-06-08 15:12:02 +0000

    arrays edited

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,7 +4,7 @@ const myArr = [0, 1, 2, 3, 4, 5, true, "Hamza"]; // mix datatypes
 const myHeros = ["Spider-Man", "Iron-Man", "Thor"]
 
 const myArr2 = new Array(1, 2, 3, 4);
-console.log(myArr[1]);
+// console.log(myArr[1]);
 
 // Array methods
 
@@ -15,7 +15,22 @@ myArr.pop(); // removes last array // [ 0, 1, 2, 3, 4, 5, true, 'Hamza', 6 ]
 myArr.unshift(9); // add data to starting of array // [ 9, 0, 1, 2, 3, 4, 5, true, 'Hamza', 6 ]
 myArr.shift(); // 0, 1, 2, 3, 4, 5, true, 'Hamza', 6 ]
 
-console.log();
+// console.log(myArr.includes(9)); // false // includes validate arrays
+// console.log(myArr.indexOf(3)); 
 
-console.log(myArr);
+const newArr = myArr.join(); // add all elemets of array into a string
 
+// console.log(myArr);
+// console.log(newArr); // 0,1,2,3,4,5,true,Hamza,6
+
+// slice, splice - which opperation munipluates
+
+console.log(" A", myArr);
+
+const myn1 = myArr.slice(1, 3);
+
+console.log(myn1);
+console.log(" B", myArr);
+
+const myn2 = myArr.splice(1, 3);
+console.log(myn2);
