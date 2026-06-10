08d4698f72b2a20e4f2499d65e2e669f48f80cef
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-06-10 16:06:02 +0000

    scopes cont.

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,15 +4,37 @@ let a = 300;
 if (true) {
     let a = 10
     const b = 20
-    console.log("Inner:", a);
+    // console.log("Inner:", a);
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];  
-}
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];  
+// }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one() {
+    const username = "Hamza-HQ"
 
+    function two() {
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+one()
+
+if (true) {
+    const username = "Hamza-HQ1"
+    if (username === "Hamza-HQ1") {
+        const website = "Youtube"
+        console.log(username + website);
+    }
+    console.log(website);
+}
+
+console.log(username);
