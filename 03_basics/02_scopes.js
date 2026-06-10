8f90c81c95417116f47eb74759ee5957d788b191
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-06-10 16:26:26 +0000

    cont.

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -31,10 +31,22 @@ one()
 if (true) {
     const username = "Hamza-HQ1"
     if (username === "Hamza-HQ1") {
-        const website = "Youtube"
+        const website = " Youtube"
         console.log(username + website);
     }
-    console.log(website);
-}
+    // console.log(website); will show error
 
-console.log(username);
+}
+// console.log(username); will show error
+
+// ++++++++++++++++++++++ Intresting ++++++++++++++++++++++
+console.log(addone(5)); // works 
+
+function addone(num) {
+    return num + 1
+}
+addTwo(5) // Error - how function is declared and called hoisting
+const addTwo = function(num) {
+    return num + 2
+}
+addTwo(5)
