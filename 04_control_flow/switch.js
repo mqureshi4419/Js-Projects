9bace485217d163b3b1009b2272c7c42ee45ed48
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-06-15 20:28:34 +0000

    switch done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break; // breaks the cases so next value doesn't show
+
+//     default:
+//         break; // 
+// }
+
+const month = "Mar";
+switch (month) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Feburary");
+        break;
+    case "Mar":
+        console.log("March");
+        break;
+    case "Apr":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case Match");
+        
+        break;
+}
