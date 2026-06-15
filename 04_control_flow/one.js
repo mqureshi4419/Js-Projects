b7e9070d40921571aea6b48118a8affee6a32f33
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-06-15 19:00:22 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,39 @@
+// if statement
+
+if (true) { //condition: true = code execute // false = code will not execute
+
+}
+
+const isUserLoggedIn = true
+const temperature = 41
+
+
+// if (temperature === 39){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+
+// < less than
+// > greater than
+// <= less than or equal
+// >= greater than or equal
+// == equal
+// != not equal (negative check)
+// === check data type (ex: 2 === "2")
+// !== negative sign check
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+//     console.log(`User power: ${power}`);
+
+const balance = 1000
+
+if (balance > 500) {
+    
+}
