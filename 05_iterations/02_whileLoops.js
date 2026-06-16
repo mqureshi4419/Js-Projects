453279c7ddbce14861f2e8372023febf737a7358
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-06-16 16:13:33 +0000

    while/ do-while loops

---
+++ 05_iterations/02_whileLoops.js
@@ -0,0 +1,22 @@
+// while loops
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2;
+}
+//Array while loops
+let myArray = ["Flash", "Bat-Man", "Super-Man"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+// do-while loops
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
