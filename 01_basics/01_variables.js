6046142655373c7ea580e62a0c499bd72824875e
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-09-03 18:09:28 +0000

    updated files

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,17 +1,44 @@
-const accountId = 144553 // const can't be changed
-let accountEmail = "Hamza@gmail.com";  // prefer to use let
-var accountPassword = "12345"; // not preferable
-accountCity = "Brooklyn"; // not a good idea
+// const accountId = 144553 // const can't be changed
+// let accountEmail = "Hamza@gmail.com";  // prefer to use let
+// var accountPassword = "12345"; // not preferable
+// accountCity = "Brooklyn"; // not a good idea
+// let accountState;
+
+// // accountId = 2; // not allowed
+
+// accountEmail = "billy@gmail.com";
+// accountPassword = "21223221";
+// accountCity = "Manhattan";
+
+// /*
+// Prefer not to use var - because of issue in block scope and functional scope
+// */
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
+// restart Sep 3rd 2026
+
+// const - cannot be reassigned
+// let - can be reassigned 
+// var - Prefer not to use = due to issue in block scope and functional scope
+// {} this is called scope 
+// let accountState; will show as undefined if ts not declared 
+
+const accountId = 1444553;
+let accountEmail = "Hamza@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
 let accountState;
 
-// accountId = 2; // not allowed
+// accountId = 2; // Const cannot be changed
 
-accountEmail = "billy@gmail.com";
-accountPassword = "21223221";
-accountCity = "Manhattan";
+accountEmail = "Bill@gmail.com" // let can be reassigned 
 
-/*
-Prefer not to use var - because of issue in block scope and functional scope
-*/
+accountPassword = "28188181"
+accountCity = "NewYork"
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+// console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
