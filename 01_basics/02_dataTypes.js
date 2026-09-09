cad283220c320bda16707872db1c8a49ee4e6de9
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-09-09 17:44:23 +0000

    Retested - Converion and OPP

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,20 +1,47 @@
+// "use strict"; // treat all JS code as newer version
+
+// // alert("3+3"); // we are using node.js not browser
+
+// // code readability should be a priority
+// // use mdn for reference or ECMA script language Specification
+
+
+// let name = "Hamza"; // String
+// let age = 25; // numercial
+// let isloggedIn = false; // boolean
+// //bigInt - for large number
+// // null = standalone value - is an object when called 
+// // undefined = value not defined but variable is declared 
+// // symbol = unique
+
+// /// object 
+
+// console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+
+// restart Sept 3rd 2026
+
 "use strict"; // treat all JS code as newer version
 
-// alert("3+3"); // we are using node.js not browser
+// alert( 3 + 3 ) // we are using nodejs, not browser
 
-// code readability should be a priority
-// use mdn for reference or ECMA script language Specification
+// code readability is important
+console.log( 3 + 3); 
+console.log("Hamza") 
 
+let name = "Hamza"
+let age = 18;
+let isloggedIn = false
+let state;
 
-let name = "Hamza"; // String
-let age = 25; // numercial
-let isloggedIn = false; // boolean
-//bigInt - for large number
-// null = standalone value - is an object when called 
-// undefined = value not defined but variable is declared 
-// symbol = unique
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value - shows as object
+// undefined =>  undefined 
+// symbol => unique
 
-/// object 
+// object 
 
-console.log(typeof null); // object
-console.log(typeof undefined); // undefined
+console.log(typeof undefined); // to know the type 
--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,78 +1,109 @@
-let score = true;
+// let score = true;
 
-// console.log(typeof (score));
+// // console.log(typeof (score));
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 
-// console.log(typeof (valueInNumber));
-// "33" = 33
+// // console.log(typeof (valueInNumber));
+// // "33" = 33
 
-// console.log(valueInNumber);
-// "33abc" -> NaN
+// // console.log(valueInNumber);
+// // "33abc" -> NaN
 
-// null = 0
-// true -> 1; false -> 0
-
-
-let isloggedIn = "Hamza";
-
-let booleanIsLoggedIn = Boolean(isloggedIn);
-
-// console.log(booleanIsLoggedIn);
-
-// "" -> false
-// "Hamza" -> true
-
-let someNumber = 33;
-
-let stringNumber = String(someNumber);
-
-// console.log(stringNumber);
-
-// ******************** Operations ********************
-
-let value = 3
-let negValue = -value;
-// console.log(negValue);
-
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**2);
-// console.log(2/2);
-// console.log(2%2); // remainder
-
-let str1 = "Hello ";
-let str2 = "Hamza";
-
-let str3 = str1 + str2;
-console.log(str3);
-
-// console.log("1" + 2); = 12
-// console.log(1 + "2"); = 12
-// console.log("1" + 2 + 2); = 122 // full conversion in string
-// console.log(1 + 2 + "2"); = 32 // 
-
-// console.log(true); = true
-// console.log(+true); = 1
-// console.log(+""); = 0 // --- not good for production
-
-// let num1, num2, num3
-// num1 = num2 = num3 = 2 + 3; --- not good for production
-
-// let gameCounter = 100;
-// ++gameCounter;
-// console.log(gameCounter);
+// // null = 0
+// // true -> 1; false -> 0
 
 
+// let isloggedIn = "Hamza";
 
-/*
-If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+// let booleanIsLoggedIn = Boolean(isloggedIn);
 
-If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+// // console.log(booleanIsLoggedIn);
 
-/*
+// // "" -> false
+// // "Hamza" -> true
 
-// Link to Study
-// https://262.ecma-international.org/#sec-abstract-operations
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+// let someNumber = 33;
+
+// let stringNumber = String(someNumber);
+
+// // console.log(stringNumber);
+
+// // ******************** Operations ********************
+
+// let value = 3
+// let negValue = -value;
+// // console.log(negValue);
+
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2**2);
+// // console.log(2/2);
+// // console.log(2%2); // remainder
+
+// let str1 = "Hello ";
+// let str2 = "Hamza";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// // console.log("1" + 2); = 12
+// // console.log(1 + "2"); = 12
+// // console.log("1" + 2 + 2); = 122 // full conversion in string
+// // console.log(1 + 2 + "2"); = 32 // 
+
+// // console.log(true); = true
+// // console.log(+true); = 1
+// // console.log(+""); = 0 // --- not good for production
+
+// // let num1, num2, num3
+// // num1 = num2 = num3 = 2 + 3; --- not good for production
+
+// // let gameCounter = 100;
+// // ++gameCounter;
+// // console.log(gameCounter);
+
+
+
+// /*
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+// /*
+
+// // Link to Study
+// // https://262.ecma-international.org/#sec-abstract-operations
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+// Restart - sept 09, 2026
+
+// Conversion and Operations 
+let score = "Hamza";
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(isloggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 = false;
+//""" => false
+// "Hamza" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+
