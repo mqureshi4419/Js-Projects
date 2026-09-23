c6a3f3dd19fbef0d02907aaf72033cafb4905ae5
Muhammad H Qureshi <93340356+mqureshi4419@users.noreply.github.com>
2026-09-23 16:46:20 +0000

    updated

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,36 +1,82 @@
-// array - mix datatypes
+// // array - mix datatypes
 
-const myArr = [0, 1, 2, 3, 4, 5, true, "Hamza"]; // mix datatypes
+// const myArr = [0, 1, 2, 3, 4, 5, true, "Hamza"]; // mix datatypes
+// const myHeros = ["Spider-Man", "Iron-Man", "Thor"]
+
+// const myArr2 = new Array(1, 2, 3, 4);
+// // console.log(myArr[1]);
+
+// // Array methods
+
+// myArr.push(6);
+// myArr.push(7); // push add to array // [ 0, 1, 2, 3, 4, 5, true, 'Hamza', 6, 7 ]
+// myArr.pop(); // removes last array // [ 0, 1, 2, 3, 4, 5, true, 'Hamza', 6 ]
+
+// myArr.unshift(9); // add data to starting of array // [ 9, 0, 1, 2, 3, 4, 5, true, 'Hamza', 6 ]
+// myArr.shift(); // 0, 1, 2, 3, 4, 5, true, 'Hamza', 6 ]
+
+// // console.log(myArr.includes(9)); // false // includes validate arrays
+// // console.log(myArr.indexOf(3)); 
+
+// const newArr = myArr.join(); // add all elemets of array into a string
+
+// // console.log(myArr);
+// // console.log(newArr); // 0,1,2,3,4,5,true,Hamza,6
+
+// // slice, splice - which opperation munipluates
+
+// console.log(" A", myArr);
+
+// const myn1 = myArr.slice(1, 3);
+
+// console.log(myn1);
+// console.log(" B", myArr);
+
+// const myn2 = myArr.splice(1, 3);
+// console.log(myn2);
+
+// Array Restart - sep 23 2026
+
+// Array is an object, datatype and colletion of multiple items
+
+//cannot be access using arbiturary string
+// creates shallow copies - need to learn what is shallow copies and deep copies 
+
+const myArr = [0, 1, 2, 3, 4, 5, true, "Ahmed"] // mix datatypes
 const myHeros = ["Spider-Man", "Iron-Man", "Thor"]
 
-const myArr2 = new Array(1, 2, 3, 4);
-// console.log(myArr[1]);
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]); // array index starts with 0
 
-// Array methods
+// Array Methods
 
-myArr.push(6);
-myArr.push(7); // push add to array // [ 0, 1, 2, 3, 4, 5, true, 'Hamza', 6, 7 ]
-myArr.pop(); // removes last array // [ 0, 1, 2, 3, 4, 5, true, 'Hamza', 6 ]
+// myArr.push(6) // push to array
+// myArr.push(7)
+// myArr.pop() // removes last value in array
 
-myArr.unshift(9); // add data to starting of array // [ 9, 0, 1, 2, 3, 4, 5, true, 'Hamza', 6 ]
-myArr.shift(); // 0, 1, 2, 3, 4, 5, true, 'Hamza', 6 ]
+myArr.unshift(9) // gets added at the starting of array 
+myArr.shift() // removes the first array
 
-// console.log(myArr.includes(9)); // false // includes validate arrays
-// console.log(myArr.indexOf(3)); 
+console.log(myArr.includes(9)); // flase or boolean type of
 
-const newArr = myArr.join(); // add all elemets of array into a string
+console.log(myArr.indexOf(9)) // -1
 
-// console.log(myArr);
-// console.log(newArr); // 0,1,2,3,4,5,true,Hamza,6
+const newArr = myArr.join()
 
-// slice, splice - which opperation munipluates
+console.log(myArr);
+console.log(newArr); // joins and turns into string
 
-console.log(" A", myArr);
+// Slice, splice
 
-const myn1 = myArr.slice(1, 3);
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3) // [ 1, 2 ]
 
 console.log(myn1);
-console.log(" B", myArr);
+console.log("B ", myArr);
 
-const myn2 = myArr.splice(1, 3);
+const myn2 = myArr.splice(1, 3) // [ 1, 2, 3 ]
+console.log("C ", myArr);
+
 console.log(myn2);
+
